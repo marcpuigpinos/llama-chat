82ea697f5072e2a74c86746022ff729c5f4ed608
Marc PB <marc.puigpinos@gmail.com>
2024-08-07 20:39:59 +0200

    Added markdown

--- gui/chat.py
+++ gui/chat.py
@@ -9,11 +9,11 @@ class Chat(ft.UserControl):
     def __init__(self):
         super().__init__(expand=True)
         self.__canvas = ft.Column(expand=True, auto_scroll=True, scroll=True)
-        self.__canvas_container = ft.Container(content=self.__canvas, expand=True)
+        self.__canvas_container = ft.Container(content=self.__canvas, expand=5)
         self.__textField = ft.TextField(label="Write a message", value="", expand=True)
         self.__send_button = ft.ElevatedButton(text="Send", icon=ft.icons.SEND, on_click=self.__send_button_on_click_event)
         self.__send_row = ft.Row(controls=[self.__textField, self.__send_button], expand=True)
-        self.__send_row_container = ft.Container(content=self.__send_row, expand=True)
+        self.__send_row_container = ft.Container(content=self.__send_row, expand=1)
         self.__chat = ft.Column(controls=[self.__canvas_container, self.__send_row_container], expand=True)
         self.__ollama = ollama_client()
 
@@ -36,8 +36,9 @@ class Chat(ft.UserControl):
         response = self.__get_ollama_response(message)
         self.__canvas.controls.append(
             ft.Container(
-                content=ft.Text(response, size=20),
-                bgcolor=ft.colors.BLUE_900,
+                #content=ft.Text(response, size=20),
+                content=ft.Markdown(value=response, selectable=True, extension_set="gitHubWeb", code_theme="atom-one-dark"),
+                #bgcolor=ft.colors.BLUE_900,
                 padding=10,
                 border_radius=10
             )
